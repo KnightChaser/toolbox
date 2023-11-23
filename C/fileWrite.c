e291bb5f9e21b1d8e76da94d5105ca79064c2003
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-11-24 08:50:50 +0900

    Create fileWrite.c

---
+++ C/fileWrite.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    FILE* file;
+    char input[1024];
+
+    file = fopen("output.txt", "a");
+
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return -1;
+    }
+
+    printf("Input...> ");
+    fgets(input, sizeof(input), stdin);
+    fprintf(file, "%s", input);
+    fclose(file);
+
+    return 0; // Return 0 to indicate success
+}
